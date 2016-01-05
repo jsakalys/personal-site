330c0bb6eed639603624a3885f2d8d7c50e2bad8
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2016-01-05 13:59:42 -0800

    redid tabs, added icons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-$('.card').flip({
-	trigger: 'manual'
+$(document).ready(function(){
+
+    // $('#tabs').tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
+    // $('#tabs li').removeClass('ui-corner-top').addClass('ui-corner-left');
+
+	$('.card').flip({
+		trigger: 'manual'
+	});
+
+	$('#view-profile').on('click', function(){
+		$('.card').flip('toggle');
+	});
+
 });
-$('#view-profile').on('click', function(){
-	$('.card').flip('toggle');
-})
