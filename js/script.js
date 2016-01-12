6b573a54fbf0fb139640d85f9f0c95d0ec28d9df
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2016-01-12 14:36:20 -0800

    changed more styling and added experience

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 
+	$(window).on('load resize', function () {
+		if (document.documentElement.clientWidth < 767) {
+	    	$('.content').height($(this).height()-125 + 'px');
+	    	console.log($('.content').height())
+		} else {
+			$('.content').height($(this).height()-75 + 'px');
+		}
+	});
+
 	$('.card').flip({
 		trigger: 'manual'
 	});
 
 	$('.profile-pane.click-button').on('click', function(){
 		$('#tabs-1.card').flip('toggle');
+	});
+
+
+	$('.resume-pane.click-button').on('click', function(){
+		$('#tabs-3.card').flip('toggle');
 	});
 
 	$('.contact-pane.click-button').on('click', function(){
